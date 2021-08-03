b421bf6c1fb6a9e95434b7a8e76cf7d9705a40ef
Piyush Dewangan <piyush0369@gmail.com>
2021-08-03 19:14:11 +0530

    Updated

--- ng2-daterangepicker.ts
+++ ng2-daterangepicker.ts
@@ -2,8 +2,6 @@
  * Public API Surface of ng2-daterangepicker
  */
 
-import { DaterangepickerComponent } from './ng2-daterangepicker.component';
-
 export * from './ng2-daterangepicker.service';
 export * from './ng2-daterangepicker.component';
 export { Daterangepicker } from './ng2-daterangepicker.module';
